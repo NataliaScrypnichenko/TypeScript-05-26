c8de98bf747a9a748a01a9470f5247bd92300ef6
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-03 22:11:32 +0300

    ok

---
+++ Lessons/typescript/main.ts
@@ -0,0 +1,11 @@
+let a:number = 100;
+// a = 'fvfdv'-дає помилку
+// Коли повертає
+function calculator(a:number,b: number):number {
+    return a + b;
+};
+ // нічого не повертає
+ function foo(a:number, b:number):void {
+    console.log(a+b);
+ };
+ 
