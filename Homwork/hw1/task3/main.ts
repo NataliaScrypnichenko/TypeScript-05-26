fe354738f524bdda4b72c4db26f296763ed2119b
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-08 21:27:40 +0300

    ok

---
+++ Homwork/hw1/task3/main.ts
@@ -0,0 +1,8 @@
+//Поля об’єкту : title ,pageCount, genre.
+
+type Book = { title:string, pageCount: number, gener: string }
+
+let book2: Book = {title: "Memory", pageCount: 200, gener: "psychology",};
+let book3: Book= {title: "Joan of Arc", pageCount: 157, gener: "historical",};
+let book1:Book = {title: "JavaScript", pageCount: 400, gener: "science",};
+
