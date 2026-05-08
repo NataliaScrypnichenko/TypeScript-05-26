03b301ec110cfbf2b745d07a18b81bfadee8ceb3
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-08 21:10:36 +0300

    ok

--- Homwork/hw1/task1/main.ts
+++ Homwork/hw1/task1/main.ts
@@ -1,8 +1,20 @@
 //   #67kfznmiMl
 // – Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 
-let arr: [number,number,[],string,{name:string,value:number},number,string,boolean
-,undefined,number] = [11, 22,[], 'hellow', {name:'masha',value:1}, 0, 'qwe', true,undefined,NaN];
+// let arr: [number,number,[],string,{name:string,value:number},number,string,boolean
+// ,undefined,number] = [11, 22,[], 'hellow', {name:'masha',value:1}, 0, 'qwe', true,undefined,NaN];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+// console.log(arr[9]);
+
+let arr: any[] = [11, 22,[], 'hellow', {name:'masha',value:1}, 0, 'qwe', true,undefined,NaN];
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
@@ -13,3 +25,4 @@ console.log(arr[6]);
 console.log(arr[7]);
 console.log(arr[8]);
 console.log(arr[9]);
+// пишем так можна звертатися до методів які призначені для масива
