fad692052edca6ebf83d084a435a92c7104a11e3
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-09 21:40:48 +0300

    ok

---
+++ Homwork/hw1/task6/main.ts
@@ -0,0 +1,15 @@
+//   #bAUsaq6LI
+// – Є змінна х, якій ви надаєте довільне числове значення.
+//  Якщо змінна x не дорівнює нулю, виведіть ‘Вірно’, інакше виведіть ‘Невірно’. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
+let strValue:string | null = prompt('enter number 1, 0, -3');
+if(strValue !== null){
+
+    let numValue=Number(strValue);//+strValue
+    if (numValue !== 0) {
+        console.log('Вірно')
+    } else {
+        console.log('Невірно')
+    };
+}
+
