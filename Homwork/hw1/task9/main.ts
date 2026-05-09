e9ec6e866e5f417dd6eaa887783cc5b4025587db
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-09 22:38:04 +0300

    ok

---
+++ Homwork/hw1/task9/main.ts
@@ -0,0 +1,34 @@
+// #KzrtqyQ
+// – Скласти розклад на тиждень за домопогою switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа, що заплановано на цей день (можна замість плану на день, назву дня англійською)
+
+let dayNumber:string | null = prompt('number day of the week');
+ if (dayNumber !== null) {
+      let key:number=+dayNumber
+switch (key) {
+    case 1:
+        console.log('Monday');
+        console.log('1.sports');
+        console.log('2.breakfast');
+        break;
+    case 2:
+        console.log('Tuesday...');
+        break;
+    case 3:
+        console.log('Wednesday...');
+        break;
+    case 4:
+        console.log('Thursday...');
+        break;
+    case 5:
+        console.log('Friday...');
+        break;
+    case 6:
+        console.log('Saturday...');
+        break;
+    case 8:
+        console.log('Sunday...');
+        break;
+    default:
+        console.log('Stop!');
+};
+ }
