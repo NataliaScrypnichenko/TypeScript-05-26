f5a65f144db22b6f8a9590f9f4b7f01d6f056e83
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-10 22:07:02 +0300

    ok

---
+++ Homwork/hw2_3/task5/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//– За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+let i = 0;
+while (i < 20) {
+    i++;
+    document.write(`<h1>Good evening!</h1>`);
+}
+;
