8e0361218a5ef620a30f0fc77daf59061a2492cd
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-10 21:58:37 +0300

    ok

---
+++ Homwork/hw2_3/task3/main.ts
@@ -0,0 +1,8 @@
+//#OeT7t3uUMFi
+//– За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+//
+let i:number=0;
+while (i < 20) {
+    document.write(`<h1>Good evening! ${i}</h1>`);
+    i++;
+};
