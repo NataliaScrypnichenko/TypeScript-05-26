7463bf49df67cc6eae53806522153fbdce31faba
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-12 21:05:34 +0300

    ок

---
+++ Homwork/hw2_3/task7/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+Object.defineProperty(exports, "__esModule", { value: true });
+let i = 0;
+for (let i = 0; i < 10; i++) {
+    document.write(`<div> lorem lorem lorem </div>`);
+}
