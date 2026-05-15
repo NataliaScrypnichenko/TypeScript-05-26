02928b264ed7781090cf8d7eb836884fad13cb1d
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-16 00:08:27 +0300

    ок

---
+++ Homwork/hw4_5/I2XsG6f/I2XsG6f.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+html >
+    lang;
+"en" >
+    charset;
+"UTF-8" >
+    Title < /title>
+    < /head>
+    < body >
+    //#I2XsG6f
+    //– створити функцію, яка обчислює та повертає площу прямокутника зі сторонами а і б
+    let;
+square = (a, b) => a * b;
+console.log(square(2, 3));
+/script>
+    < /body>
+    < /html>;
---
+++ Homwork/hw4_5/I2XsG6f/I2XsG6f.ts
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<script>
+    //#I2XsG6f
+    //– створити функцію, яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+    let square=(a,b) => a * b;
+    console.log(square(2,3));
+</script>
+</body>
+</html>
