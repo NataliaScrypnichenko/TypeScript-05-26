63a773111f229eda733c9355baffbde2cf6e99c0
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-16 00:09:33 +0300

    ок

--- Homwork/hw4_5/I2XsG6f/I2XsG6f.ts
+++ Homwork/hw4_5/I2XsG6f/I2XsG6f.ts
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<script>
+
     //#I2XsG6f
     //– створити функцію, яка обчислює та повертає площу прямокутника зі сторонами а і б
 
-    let square=(a,b) => a * b;
+    let square=(a:number,b:number):number => a * b;
     console.log(square(2,3));
-</script>
-</body>
-</html>
+
