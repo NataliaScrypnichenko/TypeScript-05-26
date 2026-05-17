cf701782f3b028f0b13690565f8b1c2c82fd0861
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-17 23:03:54 +0300

    ок

---
+++ Homwork/hw-8/#iz6emEsP2BA/main1.js
@@ -0,0 +1,33 @@
+"use strict";
+//
+// class Course {
+//     title: string;
+//     monthDuration: number;
+//     constructor(title: string, monthDuration: number) {
+//         this.title = title;
+//         this.monthDuration = monthDuration;
+//     }
+//
+// };
+//
+// class Course2 extends Course {
+//     id: number;
+//
+//     constructor(title: string, monthDuration: number, id: number) {
+//         super(title, monthDuration);
+//         this.id = id;
+//     }
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+let coursesAndDurationArray = [
+    { title: 'JavaScript Complex', monthDuration: 5 },
+    { title: 'Java Complex', monthDuration: 6 },
+    { title: 'Python Complex', monthDuration: 6 },
+    { title: 'QA Complex', monthDuration: 4 },
+    { title: 'FullStack', monthDuration: 7 },
+    { title: 'Frontend', monthDuration: 4 }
+];
+console.log(coursesAndDurationArray.map((course, index) => ({ ...course, id: index + 1 })));
+//краще інтерфейс  тому що
+// типи вони такі самі як інтерфейси але вони ще підтримують різноманітні операції юніон тобто об'єднання
+// різноманітні оператори які дозволяють створити типи із інтерфейсів
---
+++ Lessons/lesson2/DOM_типізація/dom.js
@@ -0,0 +1,48 @@
+"use strict";
+// //як знайти
+// //HTMLCollectionOf- це клас, <Element>=це дженерик-сюди відноситься будь-який тег
+//  const elementsByClassNameTarget:HTMLCollectionOf<Element>=document.getElementsByClassName('target');
+//
+// console.log(elementsByClassNameTarget);
+//
+// const elementsByTagNameDiv:HTMLCollectionOf<HTMLDivElement> = document.getElementsByTagName('div');
+// //Дженерік <HTMLDivElement>= <>= дозволяє сказати що буде знаходитися в середині HTMLCollectionOf
+// // for(const div of elementsByTagNameDiv){
+// //    //div.  = потрібно дивитися в сигнатурі(натискаєш крапку і дивишся)
+// // }
+//
+//
+// const forms:HTMLCollectionOf<HTMLFormElement> = document.forms;
+// // forms[0].name('.  можемо побачити що до ццццього типу притамані ф-ии')
+// const f1:HTMLFormElement = forms[0];
+// //дістати інпути
+// console.log(f1.username);// може бути помилка то пишемо по іншому
+// console.log(f1['username']);
+// // f1['username'].value = f1['username'];
+// let f1Element:HTMLInputElement=f1['username']
+// console.log(f1Element);
+// //  Через крапку можна також звернутися до характеристики інпута  f1Element.....
+//
+//
+// //створити елемент
+// const p:HTMLParagraphElement = document.createElement('p');
+// p.title
+// Object.defineProperty(exports, "__esModule", { value: true });
+let users = [
+    { name: 'vasya', age: 31, status: false },
+    { name: 'petya', age: 30, status: true },
+    { name: 'kolya', age: 29, status: true },
+    { name: 'olya', age: 28, status: false },
+    { name: 'max', age: 30, status: true },
+    { name: 'anya', age: 31, status: false },
+    { name: 'oleg', age: 28, status: false },
+    { name: 'andrey', age: 29, status: true },
+    { name: 'masha', age: 30, status: true },
+    { name: 'olya', age: 31, status: false },
+    { name: 'max', age: 31, status: true }
+];
+for (const user of users) {
+    const div = document.createElement('div');
+    div.innerText = user.name;
+    document.body.appendChild(div);
+}
