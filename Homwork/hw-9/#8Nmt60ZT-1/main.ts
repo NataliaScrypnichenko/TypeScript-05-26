d898fc4dd4734d7ab33d34d57632f7576dd11ccc
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-22 23:13:51 +0300

    ок

--- Homwork/hw-9/#8Nmt60ZT-1/main.ts
+++ Homwork/hw-9/#8Nmt60ZT-1/main.ts
@@ -1,4 +1,3 @@
-"use strict";
 //   #8Nmt60ZT
 //
 // – створити блок,
@@ -10,15 +9,19 @@
 // – додати цей блок в body.
 //
 // – клонувати його повністю, та додати клон в body.
-let div = document.createElement('div');
+
+
+let div:HTMLDivElement= document.createElement('div');
+
 div.classList.add('wrap');
 div.classList.add('collapse');
 div.classList.add('alpha');
 div.classList.add('beta');
-div.innerText = 'om didn\'t like going to school, and he didn\'t like working.\n' +
+div.innerText='om didn\'t like going to school, and he didn\'t like working.\n' +
     ' He liked playing and having adventures.\n' +
-    ' One Friday, he didn\'t go to school  —  he went to the river. ';
-div.style.color = 'red';
+    ' One Friday, he didn\'t go to school  —  he went to the river. '
+div.style.color ='red';
 document.body.appendChild(div);
-let copyDiv = div.cloneNode(true);
+
+let copyDiv=div.cloneNode(true);
 document.body.append(copyDiv);
