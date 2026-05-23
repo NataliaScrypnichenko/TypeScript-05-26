b373f66bac32adf7ca88391c37fb502915181bac
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-05-23 23:14:06 +0300

    ок

---
+++ Lessons/lesson2/знати/main.ts
@@ -0,0 +1,12 @@
+// Найважливіші речі, які ти вже почала розуміти:
+//
+//     getElementById() → може бути null
+// input.value працює тільки для HTMLInputElement
+// localStorage зберігає тільки string
+// JSON.parse() / JSON.stringify()
+// масиви типізуються як Type[]
+// об’єкти краще типізувати через type або interface
+// as HTMLInputElement — це приведення типу
+// дженеріки <T> роблять функції універсальними
+//
+// Це вже хороша база для TypeScript і React.
